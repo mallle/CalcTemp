f964dd2406dadae52dfdae2c600ed547c97c644a
Malene <maleneklit@hotmail.com>
2019-09-10 08:35:31 +0200

    Update index.js
    Co-Authored-By: Christian Puchinger <puchinger@playboy.de>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 let form = $('form');
 let inputFrom = $('input[name="from"]');
 let inputTo = $('input[name="to"]');
-let from = $('select[name="from"]');
+let from = $('select[name="from-unit"]');
 let to = $('select[name="to"]');
 let error = $(".error");
 
